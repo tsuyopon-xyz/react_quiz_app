097f04030de9d5092c39a35f87c1dc96b657a42e
tsuyopon-xyz <tsuyoshi4business+tsuyopon.xyz@gmail.com>
2019-10-02 20:17:30 +0900

    Attach Link of React Router to Home and Quiz components.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>ホーム</h1>
+      <Link to="/quiz">クイズページへ移動</Link>
     </div>
   );
 };
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Quiz = () => {
   return (
     <div>
-      <h1>ホーム</h1>
+      <h1>クイズ</h1>
+      <hr/>
+      <Link to="/">トップページへ</Link>
     </div>
   );
 };
