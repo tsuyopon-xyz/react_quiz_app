40a6abaf5fd043f4dd6767f78fc2516828d5dc32
tsuyopon-xyz <tsuyoshi4business+tsuyopon.xyz@gmail.com>
2019-10-03 14:32:01 +0900

    Updates

---
+++ precache-manifest.b6529081163a63eb3b5fa97d24443ba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9064fcb7400135d1e8f1d3d35e719a1",
+    "url": "/react_quiz_app/index.html"
+  },
+  {
+    "revision": "b72f001527f2df284e7e",
+    "url": "/react_quiz_app/static/css/main.b331cf76.chunk.css"
+  },
+  {
+    "revision": "2b949220d0820a980664",
+    "url": "/react_quiz_app/static/js/2.0021209e.chunk.js"
+  },
+  {
+    "revision": "b72f001527f2df284e7e",
+    "url": "/react_quiz_app/static/js/main.d11843b6.chunk.js"
+  },
+  {
+    "revision": "ca3c44063282a850a99c",
+    "url": "/react_quiz_app/static/js/runtime-main.51b8f535.js"
+  }
+]);
